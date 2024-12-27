63efa8cc2561f1ab228dbf0f6b63a150810f0ada
Aeen Karimian <aeenkarimian@gmail.com>
2024-12-27 22:18:37 +0330

    Started to learn how to use ncureses :)

---
+++ code/learning_how_to_use_n.c
@@ -0,0 +1,48 @@
+
+#include<ncurses.h>
+
+#include<stdlib.h>
+
+
+
+void printing();
+void moving_and_sleeping();
+void colouring();
+
+
+int main(void)
+{
+    initscr();
+
+    addstr("-----------------\n| codedrome.com |\n| ncurses Demo  |\n-----------------\n\n");
+    refresh();
+
+    printing();
+
+    //moving_and_sleeping();
+
+    //colouring();
+
+    addstr("\npress any key to exit...");
+    refresh();
+
+    getch();
+
+    endwin();
+
+    return EXIT_SUCCESS;
+}
+
+
+void printing()
+{
+    addstr("This was printed using addstr\n\n");
+    refresh();
+
+    addstr("The following letter was printed using addch:- ");
+    addch('a');
+    refresh();
+
+    printw("\n\nThese numbers were printed using printw\n%d\n%f\n", 123, 456.789);
+    refresh();
+}

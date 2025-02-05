1de23eb40895d23aca58d27e3868371b2f6276c3
A-sama <aeenkarimian@gmail.com>
2025-02-06 01:52:57 +0330

    make it done

--- code/rungame.sh
+++ code/rungame.sh
@@ -1,6 +1,18 @@
-gcc main.c -lncurses -lmenu -lform -lsqlite3 -o main 
+#!/bin/bash
 
+# Install required libraries
+sudo apt-get update
+sudo apt-get install libncurses5-dev libmenu-dev libform-dev libsqlite3-dev
+
+# Compile the program
+gcc main.c -lncurses -lmenu -lform -lsqlite3 -o main
+
+# Run the compiled program
 ./main
+
+# gcc main.c -lncurses -lmenu -lform -lsqlite3 -o main 
+
+# ./main
 
 # sudo apt-get install libsqlite3-dev 
 #  -lsqlite3
